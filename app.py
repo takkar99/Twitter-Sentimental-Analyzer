4cd7858ab4791764a5be336d037851bba26cf2cb
Ishaan Takkar <63262408+takkar99@users.noreply.github.com>
2021-09-20 15:15:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import tweepy
 from textblob import TextBlob
 from wordcloud import WordCloud
 import pandas as pd
-import numpy as np
 import re
 import matplotlib.pyplot as plt
 from PIL import Image
